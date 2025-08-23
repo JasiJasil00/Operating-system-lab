72fdf6386e39c11bb144fc989ac9a34d89727ffe
jasijasil <mohammedjass007@gmail.com>
2025-08-23 16:45:04 +0530

    stage6 complete

---
+++ stage6/script.sh
@@ -0,0 +1,33 @@
+cd ~
+
+cd "/mnt/d/nitc curriculum/sem5/OPERATING SYSTEM LAB/myexpos/spl"
+
+./spl "../stage6/os_startup.spl"
+
+cd  "/mnt/d/nitc curriculum/sem5/OPERATING SYSTEM LAB/myexpos/xfs-interface"
+
+
+./xfs-interface <<EOF 
+fdisk
+load --os  ../stage6/os_startup.xsm
+load --init ../stage6/square.xsm
+load --int=10 ../stage6/haltprog.xsm
+load --exhandler ../stage6/haltprog.xsm
+exit
+EOF
+
+cd  "/mnt/d/nitc curriculum/sem5/OPERATING SYSTEM LAB/myexpos/xsm"
+
+./xsm --debug --timer 0 <<EOF 
+reg r1
+c
+reg r1
+c
+reg r1
+c
+reg r1
+c
+reg r1
+c
+EOF
+
